06083c16e5e84d24b19ac04e740a04f727b3a63d
plucinsky <maros.plucinsky@gmail.com>
2026-01-17 20:24:27 +0100

    Update build

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,23 +1,21 @@
 import '../css/app.css';
 
 import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import type { DefineComponent } from 'vue';
 import { createApp, h } from 'vue';
 
 import { initializeTheme } from './composables/useAppearance';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
-const pages = import.meta.glob('./pages/**/*.vue', { eager: true });
 
 createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
-    resolve: (name) => {
-        const page = pages[`./pages/${name}.vue`];
-        if (!page) {
-            throw new Error(`Page not found: ${name}`);
-        }
-        return (page as { default: DefineComponent }).default;
-    },
+    resolve: (name) =>
+        resolvePageComponent(
+            `./pages/${name}.vue`,
+            import.meta.glob<DefineComponent>('./pages/**/*.vue'),
+        ),
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
